7ec4fabeb8fcdb4d3febb638db4c657f6c85903d
Kevin Liu <68870628+kekvinn@users.noreply.github.com>
2022-05-01 21:22:20 -0400

    Add files via upload

--- Candy_Dispenser/Candy_Dispenser.ino
+++ Candy_Dispenser/Candy_Dispenser.ino
@@ -2,64 +2,159 @@
 #include <LiquidCrystal.h>
 #include <NewPing.h>
 
-Servo myServo1;
-Servo myServo2;
-Servo myServo3;
-LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
-
 #define TRIGGER_PIN  2
 #define ECHO_PIN     3
 #define MAX_DISTANCE 400
 
+Servo myServos[3];
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-void setup() {
-  myServo1.attach(7);
-  // myServo2.attach(8);
-  // myServo3.attach(9);
-  
-  myServo1.write(0);
-  // myServo2.write(0);
-  // myServo3.write(0);
+const int buttonPin = 5;
+int buttonState = 0;
+
+String candies[3] = {"Skittles", "M&Ms", "Reese's Pieces"};
+
+int selection = 0;
+int Li = 16;
+int Lii = 0;
+int screen = 1;
+int internalTimer = 0;
+boolean dispensed = false;
+
+void setup()
+{
+  for(int i = 0; i < 3; i++)
+  {
+    myServos[i].attach(i + 7);
+    myServos[i].write(0);
+  }
 
   lcd.begin(16, 2);
+  pinMode(buttonPin, INPUT);
 }
 
-void loop() {
-
-  /**
-   for (int i = 0; i <= 180; i++)
+void loop() 
+{
+  if(screen == 1)
   {
-    myServo1.write(i); //write the i angle to the servo
-    delay(15); //delay 15ms
+    detectDistance();
+    actionListener(1);
   }
-  for (int i = 180; i >= 0; i--)
+  else
   {
-    myServo1.write(i); //write the i angle to the servo
-    delay(15); //delay 15ms
-  }
-  */
+    internalTimer++;
+    displaySelection();
+    actionListener(2);
+    delay(150);
 
-  delay(100);
-  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    if(internalTimer > 10)
+    {
+      screen = 1;
+      internalTimer = 0;
+    }
+  }
+}
+
+void detectDistance()
+{
+  unsigned int uS = sonar.ping(); 
   int distance = uS / US_ROUNDTRIP_CM;
-  lcd.setCursor(0, 0);//Place the cursor at Line 1, Column 1. From here the characters are to be displayed
-  lcd.print("Distance:");////Print Distance: on the LCD
-  lcd.setCursor(0, 1);//Set the cursor at Line 1, Column 0
-  lcd.print("             ");//Here is to leave some spaces after the characters so as to clear the previous characters that may still remain.
-  lcd.setCursor(9, 1);//Set the cursor at Line 1, Column 9.
-  lcd.print(distance);// print on the LCD the value of the distance converted from the time between ping sending and receiving. 
-  lcd.setCursor(12, 1);//Set the cursor at Line 1, Column 12.
-  lcd.print("cm");//print the unit "cm"
-
-  if(distance <= 10)
-  {
-    myServo1.write(180);
-  }
-  else if(distance > 10)
-  {
-    myServo1.write(0);
-  }
   
+  if(distance <= 8)
+  {
+    dispensed = true;
+    displayDispensing();
+    myServos[selection].write(180);
+  }
+  else if(distance > 8)
+  {
+    myServos[selection].write(0);
+    if(dispensed)
+    {
+      displayEnjoy();
+    }
+    dispensed = false;
+    displayDefault();
+  }
+}
 
+void actionListener(int screenNum)
+{
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH && dispensed == false)
+  {
+    lcd.clear();
+    incrementSelection();
+
+    switch(screenNum)
+    {
+      case 1:
+        screen = 2;
+        delay(150);
+        break;
+      case 2:
+        internalTimer = 0;
+        break;
+    }
+  }
+}
+
+void incrementSelection()
+{
+  if(selection != 2)
+  {
+    selection++;
+  }
+  else
+  {
+    selection = 0;
+  }
+}
+
+void displayDefault()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("The CanDyspenser");
+  lcd.setCursor(0, 1);;
+  lcd.print(scrollLeft("Currently dispensing: " + candies[selection]));
+  delay(150);
+}
+
+void displayDispensing()
+{
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Dispensing...");
+}
+
+void displayEnjoy()
+{
+  lcd.clear();
+  lcd.setCursor(5, 0);
+  lcd.print("Enjoy!");
+  delay(1500);
+}
+
+void displaySelection()
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Select candy:");
+  lcd.setCursor(0, 1);
+  lcd.print(candies[selection]);
+}
+
+String scrollLeft(String str)
+{
+  String result;
+  String StrProcess = "                " + str + "                ";
+  result = StrProcess.substring(Li, Lii);
+  Li++;
+  Lii++;
+  if(Li > StrProcess.length())
+  {
+    Li = 16;
+    Lii = 0;
+  }
+  return result;
 }
